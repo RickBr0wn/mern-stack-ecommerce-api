d651838b371392fb49f5eee2dfcdf0d5c550e333
Rick Brown <ricky.brown.00@gmail.com>
2022-01-19 14:12:48 +0000

    adds cart routes and endpoints

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,103 @@
 const router = require('express').Router()
+const Cart = require('../models/Cart')
+const {
+	verifyToken,
+	verifyTokenWithAuthentication,
+	verifyTokenAndAdmin
+} = require('../verifyToken')
 
-router.get('/', (req, res) => {
-	res.json({ message: '' })
+// CREATE
+router.post('/add-cart/', verifyToken, async (req, res) => {
+	const newCart = new Cart(req.body)
+
+	try {
+		const savedCart = await newCart.save()
+		return res.status(200).json(savedCart)
+	} catch (error) {
+		return res.status(500).json({
+			status: 'error',
+			error: err
+		})
+	}
+})
+
+// UPDATE
+router.put('/update/:id', verifyTokenWithAuthentication, async (req, res) => {
+	try {
+		const updatedCart = await Cart.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body
+			},
+			{ new: true }
+		)
+
+		return res.status(200).json(updatedCart)
+	} catch (err) {
+		return res.status(500).json({
+			status: 'error',
+			error: err
+		})
+	}
+})
+
+// DELETE
+router.delete(
+	'/delete/:id',
+	verifyTokenWithAuthentication,
+	async (req, res) => {
+		try {
+			await Cart.findByIdAndDelete(req.params.id)
+			return res.status(200).json({
+				status: 'delete',
+				message: `the cart ${req.params.id} has been deleted.`
+			})
+		} catch (err) {
+			return res.status(500).json({
+				status: 'error',
+				error: err
+			})
+		}
+	}
+)
+
+// GET USER CART
+router.get('/find/:id', async (req, res) => {
+	try {
+		const cart = await Cart.findOne({ userId: req.params.userId })
+		return res.status(200).json(cart)
+	} catch (error) {
+		return res.status(500).json({
+			status: 'error',
+			error: err
+		})
+	}
+})
+
+// GET PRODUCT BY ID
+router.get('/find/:id', async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id)
+		return res.status(200).json(product)
+	} catch (error) {
+		return res.status(500).json({
+			status: 'error',
+			error: err
+		})
+	}
+})
+
+// GET ALL
+router.get('/all-carts/', verifyTokenAndAdmin, async (req, res) => {
+	try {
+		const carts = await Cart.find()
+		res.status(200).json(carts)
+	} catch (err) {
+		return res.status(500).json({
+			status: 'error',
+			error: err
+		})
+	}
 })
 
 module.exports = router
